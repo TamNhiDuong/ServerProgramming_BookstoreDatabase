7db4043e9898bd667cc510e32c3df1ca49e43dd7
TamNhiDuong <yanxinyee@gmail.com>
2019-05-18 14:42:24 +0300

    Adding CRUD operations & db relationships

--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -4,25 +4,35 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Book {
+	//Unique ID
 	@Id 
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	private String title, author;
-	private int year, isbn, price;
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private int price;
 	
+	@ManyToOne
+	@JoinColumn(name = "categoryid")
+	private Category category;
 	
 	public Book() {}
 	
-	public Book(String title, String author, int year, int isbn, int price) {
+	public Book(String title, String author, int year, String isbn, int price, Category category) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+		this.category = category; 
 	}
 
 	public long getId() {
@@ -57,11 +67,11 @@ public class Book {
 		this.year = year;
 	}
 
-	public int getIsbn() {
+	public String getIsbn() {
 		return isbn;
 	}
 
-	public void setIsbn(int isbn) {
+	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
 
@@ -71,6 +81,21 @@ public class Book {
 
 	public void setPrice(int price) {
 		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category; 
+	}
+	@Override
+	public String toString() {
+		if (this.category != null)
+			return "Category [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year +  ", isbn=" + isbn +  ", price=" + price +" category =" + this.getCategory() + "]";		
+		else
+			return "Student [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price + "]";
 	}
 
 	
