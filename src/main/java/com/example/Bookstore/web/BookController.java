d3840fc9fbfca21f78bc1fe60c5021d2c5ad7a2e
TamNhiDuong <yanxinyee@gmail.com>
2019-05-29 09:53:50 +0300

    RESTFUL

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package com.example.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -7,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
@@ -61,6 +65,20 @@ public class BookController {
 		model.addAttribute("category", crepository.findAll());
 		return "editbook";
 	 }
+	 
+	    //RESTful service to get all books
+		@RequestMapping(value ="/books", method = RequestMethod.GET)
+		public @ResponseBody List<Book> bookListRest(){
+			return (List<Book>) repository.findAll();
+		}
+
+
+		//RESTful service to get books by id
+	    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+	    	return repository.findById(bookId);
+	    }       
+
 	}
 
 
